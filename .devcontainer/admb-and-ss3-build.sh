fed1b4a80f4341c7921baf64541b297f72b6388a
Elizabeth Perl (née Gugliotti) <elizabeth.gugliotti@noaa.gov>
2023-12-06 16:45:25 -0500

    Update admb-and-ss3-build.sh

--- .devcontainer/admb-and-ss3-build.sh
+++ .devcontainer/admb-and-ss3-build.sh
@@ -13,11 +13,11 @@ export PATH=$PATH:/usr/local/admb/admb
 cd examples/admb/simple
 sudo admb simple.tpl
 sudo ./simple
-cd /workflows/github-actions-test
+
 wget https://github.com/nmfs-ost/ss3-source-code/archive/main.zip
 sudo unzip main.zip -d /usr/local/bin
-sudo chmod 777 /usr/local/bin/stock-synthesis-main
-cd /usr/local/bin/stock-synthesis-main
+sudo chmod 777 /usr/local/bin/ss3-source-code-main
+cd /usr/local/bin/ss3-source-code-main
 sudo make
-mv /usr/local/bin/stock-synthesis-main/ss /workspaces/github-actions-test/ss
+
 echo 'options(repos = c(CRAN = \"https://cloud.r-project.org\"))' | sudo sh -c 'cat - >>\"${R_HOME}/etc/Rprofile.site\"'
